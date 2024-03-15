0bab20ab0f23af15b21986ddbcf4bc70b3bc99de
thonyzimm <thonysteam@gmail.com>
2024-03-14 23:09:07 -0300

    Vagrantfile

---
+++ provision.sh
@@ -0,0 +1,14 @@
+# Instalação do Docker
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Instalação do Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo usermod -aG docker jenkins
